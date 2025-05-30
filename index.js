70748cfb29cd76971ccbce50f413540610756aba
Zaki Saud <zakisaud2023@gmail.com>
2025-05-29 20:29:16 -0500

    Adding the files for the type testing- Design halfway done looking nice

---
+++ index.js
@@ -0,0 +1,11 @@
+
+    
+    // document.querySelector('.reset').addEventListener('click',function(){
+    //     console.log('Button clicked');
+        
+    // });
+
+    document.addEventListener('keydown',function(e){
+     console.log("A word is here");
+        
+    })
