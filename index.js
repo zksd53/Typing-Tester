5a72d095d27379813777f00eec994f867465473f
Zaki Saud <zakisaud2023@gmail.com>
2025-06-02 19:42:55 -0500

    B.HTML and other the file contains file work.Will be going live tomorrow

--- index.js
+++ index.js
@@ -1,47 +1,39 @@
-    document.addEventListener('keydown',function(e){
-     console.log("A word is here");
-        
-    })
-
-    const words = document.querySelector('.wordsPerMinute');
-    const accuracy = document.querySelector('.accuracy');
-    const errors = document.querySelector('.errors');
-
-    const time = document.querySelector('.time');
-    const modal = document.querySelector('.modal');
-   
-    document.querySelector('.reset').addEventListener('click',function(){
-        words = 0 ;
-        accuracy = 0;
-        errors = 0;
-        restClosed(); 
-    });
+const words = document.querySelector('.wordsPerMinute');
+const accuracy = document.querySelector('.accuracy');
+const errors = document.querySelector('.errors');
+const time = document.querySelector('.time');
+const modal = document.querySelector('.modal');
+const closeBtn = document.querySelector('.close-btn');
+ const body = document.querySelector('.body');
 
 
-    document.querySelector('.scoreCheck').addEventListener('click',function(){
-        openModal();
-    });
+document.querySelector('.scoreCheck').addEventListener('click', function () {
+    openModal();
+});
 
-    const openModal = function(){
-        modal.classList.remove('hidden');
+document.querySelector('button.reset').addEventListener('click',function(){
+    words.textContent = 'WPM: 0';
+    accuracy.textContent = 'Accuracy: 0%';
+    time.textContent = 'Time-Taken: 0.00s';
+    errors.textContent = 'Errors: 0';
+});
+
+closeBtn.addEventListener('click', function () {
+    closeModal();
+});
+
+document.addEventListener('keydown', function (e) {
+    if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+        closeModal();
     }
+});
 
-    const closeModal = function(){
-        modal.classList.add('hidden');
-    }
-    
-    document.addEventListener('keydown',function (e){
+function openModal() {
+    modal.classList.remove('hidden');
+    body.classList.add('modal-open');
+}
 
-        if(e.key === 'Escape' && !modal.classList.contains('hidden')){
-            closeModal();
-        }
-    });
-
-    let restClosed = function(){
-          document.addEventListener('keydown',function (e){
-
-        if(e.key === 'Escape' && !modal.classList.contains('hidden')){
-            closeModal();
-        }
-    });
-    }
+function closeModal() {
+    modal.classList.add('hidden');
+    body.classList.remove('modal-open');
+}
